a8d94d3cb67f1068bb33456d497e67f182542467
imad <imadatyatalah@gmail.com>
2021-12-23 18:38:19 +0100

    :tada: Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const { z } = require("zod");
+
+const app = express();
+
+app.use(express.json());
+
+const LoginSchema = z.object({
+  body: z.object({
+    email: z.string().nonempty("Email is required").email(),
+    password: z.string().nonempty("Password is required.").min(6),
+  }),
+});
+
+const validate = (schema) => (req, res, next) => {
+  try {
+    schema.parse({
+      body: req.body,
+      query: req.query,
+      params: req.params,
+    });
+
+    next();
+  } catch (err) {
+    return res.status(400).send(err.errors);
+  }
+};
+
+app.post("/login", validate(LoginSchema), (req, res) => {
+  return res.json({ ...req.body });
+});
+
+app.listen(1337, () => console.log(`> Ready on http://localhost:${1337}`));
